049771daf1f4e8930c7335ed711f566fd74d33b3
Pixelpiloten <pixelpiloten@gmail.com>
2017-02-10 19:02:46 +0100

    Renamed to serpent and added ssh-command to login to container.

--- serpent.py
+++ serpent.py
@@ -6,23 +6,30 @@ import subprocess;
 
 current_path = os.path.abspath(os.getcwd());
 app_name = os.path.basename(current_path); 
-argument = sys.argv[1];
+command = sys.argv[1];
 
 # Change dir to path where command is run
 os.chdir(current_path);
 
-if argument == 'up':
+if command == 'up':
 	subprocess.check_output(['docker-compose', 'up', '-d']);
-elif argument == 'down':
+elif command == 'down':
 	subprocess.check_output(['docker-compose', 'stop']);
-elif argument == 'provision':
+elif command == 'provision':
 	subprocess.check_output(['docker-compose', 'stop']);
 	subprocess.check_output(['docker-compose', 'rm', '-f']);
 	subprocess.check_output(['docker-compose', 'up', '--build', '-d']);
-elif argument == 'rebuild':
+elif command == 'rebuild':
 	subprocess.check_output(['docker-compose', 'stop']);
 	subprocess.check_output(['docker-compose', 'rm', '-f']);
 	subprocess.check_output(['docker-compose', 'pull']);
 	subprocess.check_output(['docker-compose', 'up', '--build', '-d']);
+elif command == 'ssh':
+    container_name = app_name +'_'+ sys.argv[2];
+    container_details = subprocess.check_output(['docker', 'ps']);
+    for item in container_details.splitlines():
+        if container_name in item:
+            container_id = item[:12];
+            subprocess.call(['docker', 'exec', '-i', '-t', container_id, '/bin/bash']);
 else:
 	print 'Invallid argument!'
